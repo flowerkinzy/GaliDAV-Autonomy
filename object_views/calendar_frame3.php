2cfb9961c5fd639851f29839cb9fe2187f860315
flowerkinzy <flowerkinzy@outlook.fr>
2015-08-13 15:37:59 +0200

    Revert "Evolution defilement (non fonctionnel)"
    This reverts commit 9c3f1689c32b976f09f65f43be284883d318883d.

--- object_views/calendar_frame3.php
+++ object_views/calendar_frame3.php
@@ -11,49 +11,59 @@ error_reporting(E_ALL);
 	</div>
 	
 	<div id='frame_calendar_core_table' class='fullspace-x'>
-
+	<?php
+	/* 
+	$hourmin=8;
+	$hourmax=19;
+	$timeintervalinmin=15;
+	$timeintervaldisplayinmin=30;
+	echo "<table id='frame_calendar_core_table' class='calendar fullspace'>";
+	for ($i = 1 ; $i <= (($hourmax-$hourmin)*60/$timeintervaldisplayinmin) -1 ; $i++){
+		echo "<tr class='calendar fullspace-x'>";
+		$Begin_H=$hourmin+floor($i*$timeintervaldisplayinmin/60);
+		$Begin_M=($i*$timeintervaldisplayinmin)%60;
+		$End_M=$Begin_M+$timeintervaldisplayinmin-1;
+		$finalres=sprintf('%02d', $Begin_H)." : ".sprintf('%02d', $Begin_M);
+		echo "<td class='fullspace-y hourcolumn' begin_hour=$Begin_H begin_min=$Begin_M end_min=$End_M><div> $finalres</div></td>";
+		for ($j_day=0; $j_day <= 4; $j_day++){
+			echo "<td class='fullspace-y daycolumn' weekday=$j_day begin_hour=$Begin_H begin_min=$Begin_M end_min=$End_M>
+				<div>  </div> </td>";
+		}
+		echo "</tr>";
+	}
+	echo"</table>"; 
+	*/
+	?>
 	<?php
 	$hourmin=8;
-	$hourmax=18;
-	$beginmin=30;
-	$endmin=45;
-	
+	$hourmax=19;
 	$timeintervalinmin=15;
 	$timeintervaldisplayinmin=30;
 	echo "<table id='calendar_core_table' class='calendar fullspace-y'>";
-	for ($beginH = $hourmin ; $beginH <= $hourmax ; $beginH++){
-		$bm=0;
-		$em=60;
-		if($beginH==$hourmin){
-			$bm=$beginmin;
+	for ($i = 1 ; $i <= (($hourmax-$hourmin)*60/$timeintervalinmin) -1 ; $i++){
+		$Begin_H=$hourmin+floor($i*$timeintervalinmin/60);
+		$Begin_M=($i*$timeintervalinmin)%60;
+		$End_M=$Begin_M+$timeintervalinmin-1;
+		$finalres=sprintf('%02d', $Begin_H)." : ".sprintf('%02d', $Begin_M);
+		echo "<tr class='calendar fullspace-x' begin_hour=$Begin_H begin_min=$Begin_M end_min=$End_M >";
+		
+		echo "<td class='fullspace-y hourcolumn' begin_hour=$Begin_H begin_min=$Begin_M end_min=$End_M><div> $finalres</div></td>";
+		for ($j_day=0; $j_day <= 4; $j_day++){
+			echo "<td class='fullspace-y daycolumn' weekday=$j_day begin_hour=$Begin_H begin_min=$Begin_M end_min=$End_M>
+				<div>  </div> </td>";
 		}
-		if($beginH==$hourmax){
-			$em=$endmin;
-		}
-		for($beginM=$bm; $beginM < $em; $beginM=$beginM+$timeintervalinmin){
-			$endM=$beginM+$timeintervalinmin;
-			echo "<tr class='calendar fullspace-x' begin_hour=$beginH begin_min=$beginM end_min=$endM >";
-			$finalres=sprintf('%02d', $beginH)." : ".sprintf('%02d', $beginM);
-			echo "<td class='fullspace-y hourcolumn' begin_hour=$beginH begin_min=$beginM end_min=$endM><div> $finalres</div></td>";
-			for ($j_day=0; $j_day <= 4; $j_day++){
-				echo "<td class='fullspace-y daycolumn' weekday=$j_day begin_hour=$beginH begin_min=$beginM end_min=$endM>
-					<div>  </div> </td>";
-			}
-			echo "</tr>";
-		}
+		echo "</tr>";
 	}
-	echo"</table>"; 	
+	echo"</table>"; 
+	
+	
 	?>
 	
 	</div>
 </div>
 <script>
 <?php
-	//$D=get_date(time()/($timeintervalinmin*60));
-	//$begin2=get_date(time()/($timeintervalinmin*60));
-	//$end2=get_date((time()/($timeintervalinmin*60))+90*60);
 	$begin=(int)floor(time()/($timeintervalinmin*60));
-	$begin=time();
 	$end=$begin+90*60; 
 	$C=new Course(new Subject('Math'),$begin,$end); 
 	$C->setRoom("C102");
--- scripts/course.js
+++ scripts/course.js
@@ -1,4 +1,32 @@
-var timeintervalinmin=15;	
+var hourmin=8;
+var hourmax=19;
+var timeintervalinmin=15;
+var timeintervaldisplayinmin=30;
+ $( document ).ready(function() {
+	$("tr.calendar").on("click","td.hourcolumn",function (){
+			$button="<button style='padding:0;position:absolute;cursor:default' ";
+			$button=$button+"begin_hour="+$(this).attr("begin_hour")+" ";
+			$button=$button+"begin_min="+$(this).attr("begin_min")+" ";
+			$button=$button+"end_min="+$(this).attr("end_min")+" ";
+			$button=$button+"> \> </button>";
+			$("#frame_calendar_core_table").append($button);
+			$( "button[begin_hour="+$(this).attr("begin_hour")+"][begin_min="+$(this).attr("begin_min")+"]" ).position({
+				my: "right",
+				at: "left top",
+				of: "tr.calendar[begin_hour="+$(this).attr("begin_hour")+"][begin_min="+$(this).attr("begin_min")+"]"
+			});
+			$( "button[begin_hour="+$(this).attr("begin_hour")+"][begin_min="+$(this).attr("begin_min")+"]" ).on("click",function(){
+				$("tr.calendar[begin_hour="+$(this).attr("begin_hour")+"][begin_min="+$(this).attr("begin_min")+"]").children().children().slideDown(1000);
+				$(this).remove();
+			});
+			$(this).parent().children().children().slideUp(1000);
+	});
+	
+	
+//FIN documentReady
+	});
+	
+	
 function getTypeName(type){
 	
 	switch(type){
@@ -21,20 +49,16 @@ function getTypeName(type){
 
 function createNewCourseElementClass(Course) { 
 	console.log("createNewCourseElementClass...");
-	var beginDate=new Date(Course.begin*1000);
-	var endDate=new Date(Course.end*1000);
+	console.dir(Course);
+	console.log("begin="+Course.begin);
+	console.log("end="+Course.end);
 	var durationInMin=Math.floor((Course.end-Course.begin)/60);
 	console.log("tr.calendar height="+$("tr.calendar").height());
 	console.log("duration="+durationInMin);
 	var height=$("tr.calendar").height()*durationInMin/timeintervalinmin;
 	
         var res="<div class='courses' style='background-color:white;z-index:2;position:absolute;min-height:"+
-			height+"px; '"+
-			" begin_hour="+beginDate.getHours()+
-			" begin_min="+beginDate.getMinutes()+
-			" end_hour="+endDate.getHours()+
-			" end_min="+endDate.getMinutes()+
-			" id="+
+			height+"px; ' id="+
 			Course.sqlId+"><p><b>"+
 			Course.subject.name;
 			if(getTypeName(Course.coursesType)!="")
@@ -54,13 +78,19 @@ function displayNewCourseElementClass(Course) {
 	var beginDate=new Date(Course.begin*1000);
 	var endDate=new Date(Course.end*1000);
 	var weekday=beginDate.getDay(); //0 is for Sunday and so on
-	console.log(beginDate.toUTCString()+" -> "+endDate.toUTCString());
+	console.log(beginDate.toGMTString()+" -> "+endDate.toGMTString());
 	console.log("weekday="+weekday);
     $("td.daycolumn[begin_hour="
 		+beginDate.getHours()+
 		"][begin_min="
-		+timeintervalinmin*Math.floor(beginDate.getMinutes()/timeintervalinmin)
+		+Math.floor(beginDate.getMinutes()/60)
 		+"][weekday="+
 		(weekday-1)%7+"]>div")
 		.append(createNewCourseElementClass(Course));
+}
+
+function test(){
+	console.log("test");
+		$("body").css("background-color","red");
+		$("body").attr("attr",1);
 }
