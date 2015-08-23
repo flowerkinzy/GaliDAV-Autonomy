13491f327f881eca663053bebb0c9ba62814217b
Orni2Thorinque <Orni2Thorinque@gmail.com>
2015-08-23 10:15:55 +0200

    Create header_menu.php
    Header menu to be included

---
+++ object_views/header_menu.php
@@ -0,0 +1,7 @@
+<div id="header-menu">
+	<ul id="view-filter">
+		<li class="view-filter-option"><a>Option 1</a></li>
+		<li class="view-filter-option"><a>Option 2</a></li>
+		<li class="view-filter-option"><a>Option 3</a></li>
+	</ul>
+</div>
