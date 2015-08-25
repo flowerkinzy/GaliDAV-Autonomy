a2ecc59cf1c3c3ee494a87fa4cc67186b73b43ee
Orni2Thorinque <Orni2Thorinque@gmail.com>
2015-08-25 10:01:16 +0200

    Create widget_zone_left.php
    Left widget bar

---
+++ object_views/widget_zone_left.php
@@ -0,0 +1,22 @@
+<div id="widget-zone-left">
+	<div class="widget-left">
+		<p class="widget-left-title"> WIdget 1</p>
+		<textbox class="widget-left-textbox">
+			Widget 1 display
+		</textbox>
+	</div>
+
+	<div class="widget-left">
+		<p class="widget-left-title"> WIdget 2</p>
+		<textbox class="widget-left-textbox">
+			Widget 2 display
+		</textbox>
+	</div>
+
+	<div class="widget-left">
+		<p class="widget-left-title"> WIdget 3</p>		
+		<textbox class="widget-left-textbox">
+			Widget 3 display
+		</textbox>
+	</div>
+</div>
