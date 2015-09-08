a111f47e87bdc37ccba4099499561c594fc1e644
Etienne <etiennelec@hotmail.fr>
2015-09-08 22:40:38 +0200

    href admin_panel

--- object_views/footer_menu.php
+++ object_views/footer_menu.php
@@ -7,7 +7,7 @@
 <a class="btn col-md-1" id="button_validate_timetable">
 	<i class="fa fa-check-square-o"></i> Valider
 </a>
-<a class="btn col-md-1 col-md-offset-6">
+<a class="btn col-md-1 col-md-offset-6" href="admin_panel2.php">
 	<i class="fa fa-cogs"></i> Admin
 </a>
 <a class="btn col-md-1">
