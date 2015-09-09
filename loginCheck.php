6468fbe8c0c267ccfbb21f2532c100fff93725c0
Etienne <etiennelec@hotmail.fr>
2015-09-09 03:36:16 +0200

    bug loginCheck

--- loginCheck.php
+++ loginCheck.php
@@ -6,15 +6,13 @@ error_reporting(E_ALL);
 require_once("classes/C_Database.php");
 require_once("functions/queries.php");
 
-$error=''; // Msg d'erreur
+$error = "Username or Password is invalid";
 
-/*if (empty($_POST['username']) || empty($_POST['password'])) {
-	$error = "Username or Password is invalid";
-	var_dump($error);
-	die();
+if (empty($_POST['username']) || empty($_POST['password'])) {
+	header("Location: login.php");
 }
 else
-{*/
+{
 	$login=$_POST['username'];
 	$password=$_POST['password'];
 	$login = stripslashes($login);
@@ -22,18 +20,23 @@ else
 	$login = pg_escape_string($login);
 
 	$dbPassword = Database::currentDB()->executeQuery(query_login($login));
-	
+
 	if(pg_num_rows($dbPassword) == 1)
 	{
+
 		if(password_verify($password, pg_fetch_result($dbPassword, "password")))
 		{
 			$_SESSION['login']=$login;
-			header("location: index.php");
+			header("Location: index.php");
+		}
+		else{
+			header("Location: login.php");
 		}
 	}
+	else{
+		header("Location: login.php");
+	}
 	
-	header("location: login.php");
-	$error = "Username or Password is invalid";
-//}
+}
 
 ?>
